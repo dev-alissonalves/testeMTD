f7bce3a6361e6dfd6007be052442720caec9610d
dev-alissonalves <alissoncc.oliveira@gmail.com>
2023-04-12 23:50:33 -0300

    completed functions and conditionals

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # testeMTD
+
+Este projeto é uma página de dados de pagamento, criada para cumprir o desafio proposto pela empresa [Agência Método]. O objetivo é criar uma página que se pareça o mais próximo possível do layout fornecido e que permita que os usuários preencham um formulário e vejam os dados no cartão atualizados em tempo real.
+
+# Tecnologias Utilizadas
+Neste projeto, utilizei as seguintes tecnologias:
+
+HTML5
+CSS3
+JavaScript
+
+# Como Executar
+A página está disponível no Github Pages. Para executá-la, basta acessar o [link](https://) e navegar pela página.
+
+# Funcionalidades
+A página permite que os usuários preencham um formulário de dados de pagamento e vejam os dados no cartão atualizados em tempo real. Além disso, os usuários recebem mensagens de erro quando o formulário é enviado, caso algum campo esteja vazio ou o número do cartão, a data de vencimento ou o CVC estejam em um formato incorreto. A página também possui um layout responsivo, que se adapta ao tamanho da tela do dispositivo em que está sendo visualizada. Por fim, a página apresenta estados de hover, ativo e focus nos elementos interativos, para uma melhor experiência do usuário.
+
+Desafios Encontrados
+Este projeto foi desafiador porque exigiu uma atenção meticulosa aos detalhes do design original. Foi necessário implementar cuidadosamente cada elemento da página, para garantir que ela ficasse o mais próximo possível do layout fornecido. Além disso, foi preciso garantir que a página funcionasse corretamente em diferentes dispositivos e tamanhos de tela, o que exigiu um planejamento cuidadoso das técnicas de design responsivo a serem utilizadas.
+
+Tempo Estimado vs. Tempo Real
+Antes de começar, estimei que este projeto levaria cerca de 10 horas para ser concluído. No entanto, após o término do projeto, verifiquei que o tempo real gasto foi de 15 horas. Essa experiência me mostrou a importância de estimar com precisão o tempo necessário para concluir um projeto e a necessidade de gerenciar meu tempo de forma eficiente para cumprir os prazos estabelecidos.
+
+Conclusão
+Este projeto foi um desafio emocionante e uma oportunidade para praticar minhas habilidades de desenvolvimento web. Espero que os usuários da página apreciem a experiência de uso e que ela cumpra com sucesso os requisitos estabelecidos pela empresa.
--- interactive-card/script.js
+++ interactive-card/script.js
@@ -1,13 +1,23 @@
 // Pegando valores dos campos
 const inputValueCardholderName = document.getElementById("info-card-name");
 const inputValueCardNumber = document.getElementById("info-card-number");
-const inputValueCardExpDateMonth = document.getElementById("info-exp-date-month");
+const inputValueCardExpDateMonth = document.getElementById(
+    "info-exp-date-month"
+);
 const inputValueCardExpDateYear = document.getElementById("info-exp-date-year");
-const inputValueCardCodeSecurity = document.getElementById("info-code-security");
+const inputValueCardCodeSecurity =
+    document.getElementById("info-code-security");
 const buttonSubmit = document.querySelector(".button-send");
 const buttonContinue = document.querySelector(".button-continue");
 const form = document.querySelector(".forms");
-const nameError = document.querySelector(".hidden-card-name");
+const success = document.querySelector(".success");
+
+//Pegando os campos de erros individuais por input
+const nameError = document.querySelector(".errorMessageName");
+const numberError = document.querySelector(".errorMessageNumber");
+const monthError = document.querySelector(".errorMessageMonth");
+const yearError = document.querySelector(".errorMessageYear");
+const cvcError = document.querySelector(".errorMessageCVC");
 
 // Pegando os campos do cartão nos quais os valores serão inseridos em tempo real
 let cardFieldName = document.querySelector(".card-name");
@@ -16,29 +26,132 @@ let cardFieldExpDateMonth = document.querySelector("#card-exp-date-month");
 let cardFieldExpDateYear = document.querySelector("#card-exp-date-year");
 let cardFieldCVC = document.querySelector(".card-info-cvc");
 
+//Validações e injeção de conteúdo nos campos do cartão
 inputValueCardholderName.oninput = () => {
     cardFieldName.innerText = inputValueCardholderName.value;
-    console.log("está funcionando...")
 };
 
 inputValueCardNumber.oninput = () => {
-    cardFieldNumber.innerText = format(inputValueCardNumber.value);
+    if (inputValueCardNumber.value.length > inputValueCardNumber.maxLength) {
+        inputValueCardNumber.value = inputValueCardNumber.value.slice(
+            0,
+            inputValueCardNumber.maxLength
+        );
+    } else {
+        cardFieldNumber.innerText = format(inputValueCardNumber.value);
+    }
 };
 
 inputValueCardExpDateMonth.oninput = () => {
-    cardFieldExpDateMonth.innerText = inputValueCardExpDateMonth.value;
+    if (
+        inputValueCardExpDateMonth.value.length >
+        inputValueCardExpDateMonth.maxLength
+    ) {
+        inputValueCardExpDateMonth.value =
+            inputValueCardExpDateMonth.value.slice(
+                0,
+                inputValueCardExpDateMonth.maxLength
+            );
+    } else {
+        cardFieldExpDateMonth.innerText = inputValueCardExpDateMonth.value;
+    }
 };
 
 inputValueCardExpDateYear.oninput = () => {
-    cardFieldExpDateYear.innerText = inputValueCardExpDateYear.value;
+    if (
+        inputValueCardExpDateYear.value.length >
+        inputValueCardExpDateYear.maxLength
+    ) {
+        inputValueCardExpDateYear.value = inputValueCardExpDateYear.value.slice(
+            0,
+            inputValueCardExpDateYear.maxLength
+        );
+    } else {
+        cardFieldExpDateYear.innerText = inputValueCardExpDateYear.value;
+    }
 };
 
 inputValueCardCodeSecurity.oninput = () => {
-    cardFieldCVC.innerText = inputValueCardCodeSecurity.value;
+    if (
+        inputValueCardCodeSecurity.value.length >
+        inputValueCardCodeSecurity.maxLength
+    ) {
+        inputValueCardCodeSecurity.value =
+            inputValueCardCodeSecurity.value.slice(
+                0,
+                inputValueCardCodeSecurity.maxLength
+            );
+    } else {
+        cardFieldCVC.innerText = inputValueCardCodeSecurity.value;
+    }
 };
 
-buttonSubmit.addEventListener("click", (evt) =>{
+buttonSubmit.addEventListener("click", (evt) => {
+    let flagError = 0;
     evt.preventDefault();
+
+    if (!inputValueCardholderName.value) {
+        nameError.classList.remove("hidden");
+    } else {
+        nameError.classList.add("hidden");
+    }
+    
+    if (!inputValueCardNumber.value) {
+        numberError.classList.remove("hidden");
+    } else if (Number(inputValueCardNumber.value.length) < 16) {
+        numberError.classList.remove("hidden");
+        numberError.textContent = "Wrong format, numbers only";
+    } else {
+        numberError.classList.add("hidden");
+    }
+
+    if (!inputValueCardExpDateMonth.value) {
+        monthError.classList.remove("hidden");
+    } else if (
+        Number(inputValueCardExpDateMonth.value) < 1 ||
+        Number(inputValueCardExpDateMonth.value) > 12
+    ) {
+        monthError.classList.remove("hidden");
+        monthError.textContent = "1-12 only";
+    } else {
+        monthError.classList.add("hidden");
+    }
+
+    if (!inputValueCardExpDateYear.value) {
+        yearError.classList.remove("hidden");
+    } else if (
+        Number(inputValueCardExpDateYear.value) < 23 ||
+        Number(inputValueCardExpDateYear.value) > 33
+    ) {
+        yearError.classList.remove("hidden");
+        yearError.textContent = "23-33 only"; // 10 anos geralmente é a validade de um cartão de crédito convencional
+    } else {
+        yearError.classList.add("hidden");
+    }
+
+    if (!inputValueCardCodeSecurity.value) {
+        cvcError.classList.remove("hidden");
+    } else if (inputValueCardCodeSecurity.value.length > 3){
+        cvcError.classList.remove("hidden");
+        cvcError.textContent = "CVC Invalid";
+    }else {
+        cvcError.classList.add("hidden");
+    }
+        
+    if (
+        inputValueCardholderName.value &&
+        inputValueCardNumber.value &&
+        inputValueCardExpDateMonth.value &&
+        inputValueCardExpDateYear.value &&
+        inputValueCardCodeSecurity.value
+    ) {
+        form.classList.add("hidden-div");
+        success.classList.remove("hidden-div");
+    }
+});
+
+buttonContinue.addEventListener("click", () => {
+    location.reload();
 });
 
 function format(value) {
